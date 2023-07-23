dcef2a36eeca18893744b5436340226fdfb4a78c
Marius Golabski <mgolabski@gmail.com>
2023-07-23 15:54:56 +0200

    Refactor FavoriteButton component: Add state for favorite status and implement toggle functionality

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,17 +1,19 @@
+import { useState } from "react";
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
 export default function FavoriteButton() {
-  // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setFavorite] = useState(false);
+
+  function toggleFavorite() {
+    setFavorite(!isFavorite);
+  }
 
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
+      onClick={toggleFavorite}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
